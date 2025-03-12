0f8b97f9c546ec40372d04e0c19fd776b9e6cd58
Agamjot Sodhi <agamjotsodhi@hotmail.com>
2025-03-11 23:19:29 -0400

    Updated Styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,28 +2,31 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import CurrentUserContext from "../context/CurrentUserContext";
 import "../styles/Home.css";
-import "../styles/Buttons.css"
+import "../styles/Buttons.css";
 
 const Home = () => {
-  const { user } = useContext(CurrentUserContext); 
+  const { user } = useContext(CurrentUserContext);
 
   return (
     <div className="home-container">
-      {/* Title */}
+      {/* h1 Welcome Message */}
       <h1 className="home-title">Welcome, Let's Plan a Trip!</h1>
 
-      {/* Home page image */}
-      <img src="/assets/Bus.png" alt="Hot Air Balloon" className="landing-image" />
+      {/* Centered Image */}
+      <div className="home-image">
+        <img src="/assets/Home.png" alt="Home page image, airplane flying around globe" />
+      </div>
 
-
-      {/* Buttons at the bottom */}
-      <div className="home-buttons">
-        <Link to="/plan-trip">
-          <button className="btn-one" id="plan-trip-btn">Plan a Trip</button>
-        </Link>
-        <Link to="/my-trips">
-          <button className="btn-one" id="my-trips-btn">My Trips</button>
-        </Link>
+      {/* Button Container with Box Styling */}
+      <div className="home-buttons-container">
+        <div className="home-buttons">
+          <Link to="/plan-trip">
+            <button className="btn-primary">Plan a Trip</button>
+          </Link>
+          <Link to="/my-trips">
+            <button className="btn-primary">My Trips</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
